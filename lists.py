4ecf6648f30c58526936b9c4baf368fcf8d335d1
Dakota Dunham-Brown <dakotacdb@Dakotas-MBP.attlocal.net>
2019-11-12 21:59:49 -0500

    adding python tutorials and code alongs

---
+++ lists.py
@@ -0,0 +1,24 @@
+friends = ["Kate", "Alicia", "Will", "Lili", "Jeff"]
+lucky_numbers = [3, 7, 11, 23, 42, 56]
+
+#print(friends)
+#print(friends[4])
+#print(friends[3:])
+#print(friends[1:3])
+
+#friends.extend(lucky_numbers) #extend will concatonate the two lists
+#friends.append("Wallaby")  #append will add to the end
+#friends.insert(1, "LuLu") #insert will take two params, the first is the position, the second is the item to be added
+#friends.remove("Will") #clear will remove the selected item from the list
+#friends.clear() #clear will reset the entire list
+#friends.pop() #pop will pop the last element from the list
+#friends.index("Kevin") #index will tell you wether an item is in a list or not. if it is, it will provide the index number
+#print(friends.index("Will")) # index now tells me that Will is at index number 2
+#print(friends.count("Alicia")) #count tells me how many times Alicia shows up in my list
+
+friends.sort() #sort will put this list in alphabetic order
+lucky_numbers.sort()
+
+
+print(friends)
+print(lucky_numbers)
